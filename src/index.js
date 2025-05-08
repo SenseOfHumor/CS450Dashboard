d9ba93c2f40c86e03b0e5d343afd9f26458c2480
Jason Cordova <jasoncordova.edu@gmail.com>
2025-05-08 19:20:24 -0400

    Merge fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-import './index.css';
-=======
 import "./index.css";
->>>>>>> 1b7e9d79c8145874bfac1524a367bf5eca15999a
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
